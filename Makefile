e7bd32555b83bf0e6410b8a2c169f56747cb891d
Robert Nix <mischanix@gmail.com>
2015-08-05 01:14:58 -0500

    Make MonoDevelop solution

--- Makefile
+++
@@ -1,7 +0,0 @@
-all: main
-
-main:
-	mcs main.cs -pkg:dotnet -r:lib/Mono.Cecil.dll -r:lib/Mono.Cecil.Rocks.dll
-
-clean:
-	rm -f main.exe
---
+++ extractor.cs
@@ -0,0 +1,7 @@
+using System;
+
+static class Extractor {
+	static void Main(string[] args) {
+		Console.WriteLine("Hearthstone Protobuf Extractor --- ");
+	}
+}
