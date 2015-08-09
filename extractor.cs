c850c2f835f2d6d5e2f03cb8c2295d92a6c5910f
Robert Nix <mischanix@gmail.com>
2015-08-09 03:48:59 -0500

    Added AST nodes for generating .proto syntax

--- extractor.cs
+++ extractor.cs
@@ -2,6 +2,10 @@ using System;
 
 static class Extractor {
 	static void Main(string[] args) {
-		Console.WriteLine("Hearthstone Protobuf Extractor --- ");
+		if (args.Length == 0) {
+			Console.Write("Hearthstone Protobuf Extractor --- ");
+			Console.WriteLine("Usage: proto-extractor [input dlls...]");
+			return;
+		}
 	}
 }
