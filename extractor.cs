171fe4cdd7f79946c0e0cfb7bb1ff07e8c094150
Robert Nix <mischanix@gmail.com>
2015-08-17 03:49:15 -0500

    Add type processor and method analyzer

--- extractor.cs
+++ extractor.cs
@@ -1,11 +1,24 @@
 using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+using Mono.Cecil.Rocks;
 
 static class Extractor {
 	static void Main(string[] args) {
 		if (args.Length == 0) {
 			Console.Write("Hearthstone Protobuf Extractor --- ");
 			Console.WriteLine("Usage: proto-extractor [input dlls...]");
+			Console.WriteLine("output is written to the working directory");
 			return;
 		}
+
+		var allTypes = new List<TypeDefinition>();
+		foreach (var arg in args) {
+			allTypes.AddRange(ModuleDefinition.ReadModule(arg).GetAllTypes());
+		}
+
+		var decompiler = new ProtobufDecompiler();
+		decompiler.ProcessTypes(allTypes);
+		decompiler.WriteProtos();
 	}
 }
